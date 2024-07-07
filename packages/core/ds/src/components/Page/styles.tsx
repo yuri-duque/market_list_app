7cc85944f8cecc1e1017b8bf5a57ff18d77bf916
yuri-duque <yurithielmann83@gmail.com>
2024-07-07 18:44:20 -0300

    creating page component

--- packages/core/ds/src/components/Page/styles.tsx
+++ packages/core/ds/src/components/Page/styles.tsx
@@ -4,5 +4,6 @@ export const Container = styled.View<Pick<PageProps, "noPadding">>`
   width: 100%;
   height: 100%;
   padding: ${({noPadding, theme}) =>
-    noPadding ? 0 : theme.sizeVariations.XS}px;
+    noPadding ? 0 : theme.sizeVariations.S}px;
+  background-color: ${({theme}) => theme.colors.background};
 `;
--- packages/core/ds/src/components/index.ts
+++ packages/core/ds/src/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./Button";
+export * from "./Card";
+export * from "./Input";
+export * from "./Label";
 export * from "./Page";
+export * from "./Spacing";
 export * from "./Typography";
