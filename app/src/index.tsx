1162b7caedf72bd2b6d3738ca5bec9a746d911e8
yuri-duque <yurithielmann83@gmail.com>
2024-07-11 22:13:47 -0300

    configuring toast message

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
+import Toast from "react-native-toast-message";
 import {LoadingProvider, ThemeProvider} from "@core/ds";
 import {Routes} from "./routes";
 
 export const App = () => {
   return (
-    <ThemeProvider>
-      <LoadingProvider>
-        <Routes />
-      </LoadingProvider>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <LoadingProvider>
+          <Routes />
+        </LoadingProvider>
+      </ThemeProvider>
+      <Toast />
+    </>
   );
 };
